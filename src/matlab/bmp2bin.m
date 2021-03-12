d7564c095b92a0e6f3eaa159ff13f7f059a95549
oscarbj <oscarbjorkgren@gmail.com>
2021-03-12 16:00:31 +0200

    Changed conversion script to use data from single channel

--- src/matlab/bmp2bin.m
+++ src/matlab/bmp2bin.m
@@ -2,13 +2,13 @@ input_bmp = '../../res/ff.bmp'; % Conversion input
 output_bin = '../../res/ff-out.bin'; % Conversion output
 
 I = imread(input_bmp);
-%I = I(:,:,3);
+I = I(:,:,3);
 %info = imfinfo(input_bmp);
 %info.BitDepth
 J = uint8(I);
 K = double(J);
-[M,N] = size(I);
-M = reshape(K,[M*N 1]);
+[M,N] = size(J);
+M = reshape(J,[M*N 1]);
 M = dec2bin(M,8);
 fid = fopen(output_bin,'wb');
 
